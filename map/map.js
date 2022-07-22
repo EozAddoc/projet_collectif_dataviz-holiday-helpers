06b8ae0da378c2caf520085acbdd2b13dbb7c5a2
Zoe Codda <addoceoz@macbook-air-de-zoe.home>
2022-07-22 21:16:01 +0200

    search

--- map/map.js
+++ map/map.js
@@ -31,37 +31,20 @@ myPosition();
 
   //markers NOT WORKING !!!!
    var marker = L.marker([48.876209335951565, 2.3198562079823546]).addTo(map);
-//   var circle = L.circle([51.508, -0.11], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(map);
-//   L.Control.geocoder().addTo(map);
+
+    L.Control.geocoder().addTo(map);
   
-//  // Control 2: This add a scale to the map
-//  L.control.scale().addTo(map);
+// Control 2: This add a scale to the map
+ L.control.scale().addTo(map);
 
 //  // Control 3: This add a Search bar
-// 	 var searchControl = new L.esri.Controls.Geosearch().addTo(map);
+ 	 var searchControl = new L.esri.Controls.Geosearch().addTo(map);
 
-// 	 var results = new L.LayerGroup().addTo(map);
+ 	 var results = new L.LayerGroup().addTo(map);
 
-// 	   searchControl.on('results', function(data){
-// 		 results.clearLayers();
-// 		 for (var i = data.results.length - 1; i >= 0; i--) {
-// 		   results.addLayer(L.marker(data.results[i].latlng));
-// 		 }
-// 	   });
-
-//   // the popups to tell you location
-//   var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
-
-// map.on('click', onMapClick);
+  	   searchControl.on('results', function(data){
+	 results.clearLayers();
+	 for (var i = data.results.length - 1; i >= 0; i--) {
+		   results.addLayer(L.marker(data.results[i].latlng));
+	 }
+ 	   });
