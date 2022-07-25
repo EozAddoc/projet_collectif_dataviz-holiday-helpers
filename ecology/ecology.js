a3f71206f60e7c393d0909c75e850010ad82ede9
Zoe Codda <addoceoz@MacBook-Air-de-Zoe.local>
2022-07-25 11:59:33 +0200

    Zoe

--- ecology/ecology.js
+++ ecology/ecology.js
@@ -1,11 +1,11 @@
 var banner = document.querySelector("banner");
 
 //THe data
-const airAPi = "https://api.ambeedata.com/latest/by-lat-lng?lat=12&lng=77";
+const airAPi = " https://api.ambeedata.com/latest/by-city?city=${city}";
 const weatherAPi =
-  "https://api.ambeedata.com/weather/latest/by-lat-lng?lat=12&lng=77";
+  "https://api.ambeedata.com/weather/latest/by-lat-lng?lat=12&lng=17";
 const pollenApi =
-  "https://api.ambeedata.com/latest/pollen/by-lat-lng?lat=12.9889055&lng=77.574044";
+  "https://api.ambeedata.com/latest/pollen/by-lat-lng?lat=12&lng=17";
 
 //Map stuff
 
@@ -34,7 +34,7 @@ L.Control.geocoder().addTo(map);
 L.control.scale().addTo(map);
 
 //  // Control 3: This add a Search bar
-var searchControl = new L.esri.Controls.Geosearch().addTo(map);
+var searchControl = new L.Controls.Geosearch().addTo(map);
 
 //Geolocalisation
 async function myPosition() {
@@ -65,21 +65,19 @@ async function air() {
       "Content-type": "application/json",
     },
   });
-  var myIcon = L.icon({
-    iconUrl: 'co2.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
+
   const data = await response.json();
   console.log(data);
   let aQI = await data.stations[0].aqiInfo.category;
   console.log(aQI);
+  let ozone = await data.stations[0].OZONE;
+  let city = await data.stations[0].city
   let coord = await data.stations[0];
   console.log(coord);
   const { lat, lng } = coord;
+ 
   var myIcon = L.icon({
-    iconUrl: 'co2.png',
+    iconUrl: 'markers/co2.png',
     iconSize: [38, 95],
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76],
@@ -101,43 +99,23 @@ async function getMeteo() {
   const data = await res.json();
   let temperature = await data.data.temperature;
   let coord = await data.data;
-  const { lat, lng } = data;
+  const { lat, lng } = coord;
   var myIcon = L.icon({
     iconUrl: 'markers/hot.png',
     iconSize: [38, 95],
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76],
 });
-marker.setLatLng([lat, lng],{icon: myIcon});
+marker.setLatLng([lat, lng]);
 }
 //getMeteo();
-
-
-async function fire() {
-  const res = await fetch(
-    "https://api.ambeedata.com/latest/fire?lat=12&lng=77",
-    {
-      method: "GET",
-      headers: {
-        "x-api-key":
-          "0b6971e42ac3de1fc7283214f67e40a0638921c67ffee69fd130f4bd301f6e24",
-        "Content-type": "application/json",
-      },
-    }
-  );
-  const data = await res.json();
-  const { message } = data;
-
-  console.log(data);
-}
-//fire()
-
 
 // async function flights() {
 //   
 // flights();
 
 async function pollen() {
+  console.log("hi")
   const res = await fetch(pollenApi, {
     method: "GET",
     headers: {
@@ -147,18 +125,14 @@ async function pollen() {
     },
   });
   const data = await res.json();
+  let risk = await data.data[0].Risk;
   console.log(data)
   const { lat, lng } = data;
-  let risk = await data.data[0].Risk;
-  var myIcon = L.icon({
-    iconUrl: 'markers/pollen.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
-marker.setLatLng([lat, lng],{icon: myIcon});
+  
+ 
+  marker.setLatLng([lat, lng]);
 }
-//pollen()
+
 
 //getTime
 function renderTime() {
